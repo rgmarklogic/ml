c399f6c083359223ff3b580f8ab1eb1118ec993a
Jeremy Osborne <jeremywosborne@gmail.com>
2014-05-14 15:47:48 -0700

    Update.

--- server.js
+++ server.js
@@ -12,10 +12,27 @@ if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
+var messages = [
+    "hello world",
+    "marklogic",
+    "goodbye world!!!!!"
+];
+app.get("/messages", function(req, res) {
+    res.send(messages);
+});
+app.post("/messages", function(req, res) {
+    var messageCandidate = req.param("message");
+    messages.push(messageCandidate);
+    res.send({
+        message: messageCandidate
+    });
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 app.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+
 
 
